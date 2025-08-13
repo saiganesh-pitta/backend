c8c99fb1a845a8ec4064b0d5d07538db69689129
saiganesh-pitta <saiganeshpitta5@gmail.com>
2025-08-13 18:03:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ const getDB=()=>{
 const rootDir=path.dirname(require.main.filename);
 
 app.use(bodyparser.json())
-app.use(cors({ origin: 'http://localhost:5173',credentials: true,}))
+app.use(cors({ origin:'https://housedotrental.netlify.app/',credentials: true,}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
